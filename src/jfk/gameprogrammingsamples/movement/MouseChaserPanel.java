bebfcb1a56eea411823bcb87ea303c732183b722
Jakob Farian Krarup <kr@rup.dk>
2020-12-03 12:04:53 +0100

    tweaks and optimizations

--- src/jfk/gameprogrammingsamples/movement/MouseChaserPanel.java
+++ src/jfk/gameprogrammingsamples/movement/MouseChaserPanel.java
@@ -14,13 +14,12 @@ public class MouseChaserPanel extends JPanel implements MouseMotionListener {
 	
 	//private variables 
 	Image tractorImage = null; 										
-	Point currentMousePosition = new Point(); 						// stores the mouse position from last movement
-	Point tractorPosition = new Point(200,200);
-	int currentTractorImage = 0;
-	int tractorWidth = 128, tractorHeight = 128; 						// half width and -height, used to draw the balloon centered on the position
-	int tractorImageIndex = 0;
-	int speed = 2;
-	Font font = new Font("Arial", 0, 25);							//a bigger font, to make it easier to read
+	Point currentMousePosition = new Point(); 		// stores the mouse position from last detected mouse move
+	Point tractorPosition = new Point(200,200);		// sets the tractor's starting position
+	int tractorWidth = 128, tractorHeight = 128; 	// height and width of the image of the tractor 
+	int tractorImageIndex = 0;						// which image in the sprite sheet to use based on current direction
+	int speed = 2;									//how many pixels to move on each axis per update
+	Font font = new Font("Arial", 0, 25);		 	//a bigger font, to make it easier to read
 
 
 	//main method, to instantiate and add the JPanel to a window (JFrame) 
@@ -40,7 +39,9 @@ public class MouseChaserPanel extends JPanel implements MouseMotionListener {
 
 	public MouseChaserPanel() {
 		tractorImage = loadImage("/movement/tractor_spritesheet.png"); 		// load tractor spritesheet with tractor pointing in all 8 directions
-		Image cursorImg = loadImage("/movement/x.png");						//load the "x" to use for mouse cursor
+		
+		//load the "x" to use for mouse cursor
+		Image cursorImg = loadImage("/movement/x.png");		
 
 		// Create a new cursor from the 'x' image
 		Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "green 'X' cursor");
@@ -60,37 +61,37 @@ public class MouseChaserPanel extends JPanel implements MouseMotionListener {
 		}
 	}
 	
-	private void moveTractorTowardMouse() {
-		
-		//find out what direction to move on the two axes
-		int distanceToMouseX = currentMousePosition.x - tractorPosition.x ;
-		int distanceToMouseY = currentMousePosition.y - tractorPosition.y ;
-		
-		//find out whether that is positive, negative or zero (mathematical 'sign' operation)
-		int directionXToMouse = (int)Math.signum( distanceToMouseX );
-		int directionYToMouse = (int)Math.signum( distanceToMouseY );
-		
-		//find out which image to use, by looking at the direction on both axes
-		if(directionXToMouse == 0 && directionYToMouse == -1) { tractorImageIndex = 0;}
-		if(directionXToMouse == 1 && directionYToMouse == -1) { tractorImageIndex = 1;}
-		if(directionXToMouse == 1 && directionYToMouse == 0) { tractorImageIndex = 2;}
-		if(directionXToMouse == 1 && directionYToMouse == 1) { tractorImageIndex = 3;}
-		if(directionXToMouse == 0 && directionYToMouse == 1) { tractorImageIndex = 4;}
-		if(directionXToMouse == -1 && directionYToMouse == 1) { tractorImageIndex = 5;}
-		if(directionXToMouse == -1 && directionYToMouse == 0) { tractorImageIndex = 6;}
-		if(directionXToMouse == -1 && directionYToMouse == -1) { tractorImageIndex = 7;}
-		
-		//add the direction to the current position, to find the tractor's new position
-		Point newTractorPosition = new Point(tractorPosition.x + directionXToMouse*speed, tractorPosition.y + directionYToMouse*speed);
-		
-		//ensure the tractor doesn't "overshoot" 
-		//(e.g. if it's 2 pixels away, and the speed is 3, then it would overshoot the target by 1 pixel).
-		if(Math.abs(newTractorPosition.x - currentMousePosition.x) <= speed) {newTractorPosition.x = currentMousePosition.x;}
-		if(Math.abs(newTractorPosition.y - currentMousePosition.y) <= speed) {newTractorPosition.y = currentMousePosition.y;}
-		
-		//update the tractor's position
-		tractorPosition = newTractorPosition;
-	}
+private void moveTractorTowardMouse() {
+	
+	//find out what direction to move on the two axes
+	int distanceToMouseX = currentMousePosition.x - tractorPosition.x ;
+	int distanceToMouseY = currentMousePosition.y - tractorPosition.y ;
+	
+	//find out whether that is positive, negative or zero (mathematical 'sign' operation)
+	int directionXToMouse = (int)Math.signum( distanceToMouseX );
+	int directionYToMouse = (int)Math.signum( distanceToMouseY );
+	
+	//find out which image to use, by looking at the direction on both axes
+	if(directionXToMouse == 0 && directionYToMouse == -1) { tractorImageIndex = 0;}
+	else if(directionXToMouse == 1 && directionYToMouse == -1) { tractorImageIndex = 1;}
+	else if(directionXToMouse == 1 && directionYToMouse == 0) { tractorImageIndex = 2;}
+	else if(directionXToMouse == 1 && directionYToMouse == 1) { tractorImageIndex = 3;}
+	else if(directionXToMouse == 0 && directionYToMouse == 1) { tractorImageIndex = 4;}
+	else if(directionXToMouse == -1 && directionYToMouse == 1) { tractorImageIndex = 5;}
+	else if(directionXToMouse == -1 && directionYToMouse == 0) { tractorImageIndex = 6;}
+	else if(directionXToMouse == -1 && directionYToMouse == -1) { tractorImageIndex = 7;}
+	
+	//add the direction to the current position, to find the tractor's new position
+	Point newTractorPosition = new Point(tractorPosition.x + directionXToMouse*speed, tractorPosition.y + directionYToMouse*speed);
+	
+	//ensure the tractor doesn't "overshoot" 
+	//(e.g. if it's 2 pixels away, and the speed is 3, then it would overshoot the target by 1 pixel).
+	if(Math.abs(newTractorPosition.x - currentMousePosition.x) <= speed) {newTractorPosition.x = currentMousePosition.x;}
+	if(Math.abs(newTractorPosition.y - currentMousePosition.y) <= speed) {newTractorPosition.y = currentMousePosition.y;}
+	
+	//update the tractor's position
+	tractorPosition = newTractorPosition;
+}
 	
 
 	private void waitAShortInterval() {
